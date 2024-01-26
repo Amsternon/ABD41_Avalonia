0b71d15f248df7c4f1368bbe78e24107872079f1
Amsternon <vitsnork342@gmail.com>
2024-01-26 20:38:48 +0700

    Add new feature

--- AvaloniaApplication1/MainWindow.axaml.cs
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -16,14 +16,14 @@ public partial class MainWindow : Window
         string password = TextBox1.Text;
 
         // Проверка логина и пароля для врача
-        if (login == "admin" && password == "admin")
+        if (login == "admin" && password == "12345")
         {
             AdminWorm admin = new AdminWorm();
             admin.Show();
             this.Close();
         }
         // Проверка логина и пароля для пациента
-        else if (login == "client" && password == "client")
+        else if (login == "client" && password == "54321")
         {
             ClientWorm client = new ClientWorm();
             client.Show();
